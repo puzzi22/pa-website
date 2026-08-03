e219afd6ff7428feb44b3ccef68a7a8cb9b0793f
Pierre-Antoine Tremblay <patremblay@iMac.local>
2026-08-03 20:00:05 +0200

    new CD

--- src/assets/scripts/recording-gallery.js
+++ src/assets/scripts/recording-gallery.js
@@ -48,6 +48,7 @@ import img51 from "../images/recordings/51.webp";
 import img52 from "../images/recordings/52.webp";
 import img53 from "../images/recordings/53.webp";
 import img54 from "../images/recordings/54.webp";
+import img54 from "../images/recordings/55.webp";
 import img6 from "../images/recordings/6.webp";
 import img7 from "../images/recordings/7.webp";
 import img8 from "../images/recordings/8.webp";
@@ -58,6 +59,7 @@ const recordingsContainer = document.getElementById("recordings-container");
 
 // Array of image filenames (adjust these according to your filenames)
 const imageFilenames = [
+  img55,
   img54,
   img53,
   img52,
